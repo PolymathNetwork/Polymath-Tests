b2c254e3395fb5bec7ae32aaba7a73fd05f6833d
Adrien Denat <adrien.denat@gmail.com>
2018-09-20 17:49:38 -0300

    feat(issuer): keep on replacing xpath selectors with more reliable ones using id

--- objects/pages/withToken/sto/abstract.ts
+++ objects/pages/withToken/sto/abstract.ts
@@ -1,6 +1,6 @@
 import { Locator, By } from "framework/helpers";
 import { nav, PageWithToken } from "objects/pages/withToken/base";
 
-@nav(By.xpath('.//li[./p[text()="STO"]]')) export abstract class BaseSto extends PageWithToken {
-    protected featureSelector: Locator = By.xpath('self::*[.//li[@class="active"][./p[text()="STO"]]]');
+@nav(By.xpath('//*[@id="sto-nav-link"]')) export abstract class BaseSto extends PageWithToken {
+    protected featureSelector: Locator = By.xpath('self::*[.//li[@class="active"]]');
 }
